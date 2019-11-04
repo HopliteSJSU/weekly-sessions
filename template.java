97544f7cc2fba759b26688c22da5cee8a28ebe5c
Saket Thakare <saketthakare@gmail.com>
2019-11-03 18:06:50 -0800

    Add solution template

---
+++ template.java
@@ -0,0 +1,12 @@
+/**
+ * Question: <problem-name>
+ * Link: <problem-link>
+ * Hoplite Session: MM/DD/YYY
+ * 
+ * Time Complexity: <time-complexity>
+ * Space Complexity: <space-complexity
+ */
+
+class Solution {
+    
+}
