8c8a1980772046cab163e7fcbcc5a8e8f1eee9ca
Saket Thakare <saketthakare@gmail.com>
2019-11-24 11:36:03 -0800

    Add 11-15 session

---
+++ session-11-15-2019-problem-207-course-schedule.java
@@ -0,0 +1,62 @@
+/**
+ * Question: 207. Course Schedule 
+ * Link: https://leetcode.com/problems/course-schedule/ 
+ * Hoplite Session: 11/15/2019
+ * 
+ * Time Complexity: O(V * V)
+ * Space Complexity: O(E + V)
+ * 
+ * We use toposort to find all the possible courses that can be completed.
+ * 
+ */
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+        int[][] adjacencyMatrix = new int[numCourses][numCourses];
+        int[] indegree = new int[numCourses];
+
+        // Construct adjacencyMatrix and calculate indegree for every node
+        for (int i = 0; i < prerequisites.length; i++) {
+            int course = prerequisites[i][0];
+            int prerequisite = prerequisites[i][1];
+            if (adjacencyMatrix[prerequisite][course] == 0) {
+                indegree[course]++;
+            }
+            adjacencyMatrix[prerequisite][course] = 1;
+        }
+
+        Queue<Integer> queue = new LinkedList();
+
+        /**
+         * Initialize queue with courses having no prerequisites
+         */
+        int possibleCoursesCount = 0;
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int currentCourse = queue.poll();
+            possibleCoursesCount++;
+
+            /**
+             * Remove current course prerequisites as a dependency from
+             * other nodes and update indegree.
+             */
+            for (int i = 0; i < numCourses; i++) {
+                if (adjacencyMatrix[currentCourse][i] != 0) {
+                    indegree[i]--;
+                    if (indegree[i] == 0) {
+                        queue.add(i);
+                    }
+                }
+            }
+
+        }
+
+        return possibleCoursesCount == numCourses;
+    }
+}
---
+++ session-11-15-2019-problem-399-evaluate-division.java
@@ -0,0 +1,102 @@
+/**
+ * Question: 399. Evaluate Division
+ * Link: https://leetcode.com/problems/evaluate-division/ 
+ * Hoplite Session: 11/15/2019
+ * 
+ * Time Complexity: O(K * E)
+ * Space Complexity: O(E)
+ * 
+ * K is the number of queries, and E is the number of edges
+ * 
+ * Approach: 
+ * (a/b) x (b/c) x (c/d) = a/d
+ * We use this property to find a/d. This can be done by finding a path between
+ * a to d and evaluating the division on the way.
+ * 
+ * We find a path by creating a weighted graph. For eg. if 'a/b = 2' we store an edge from
+ * a to b with a value 2 and an edge from b to a with value 1/2. 
+ * 
+ */
+
+class Solution {
+    class Node {
+        public String val;
+        HashMap<Node, Double> next;
+        Node(String val){
+            this.val = val;
+            next = new HashMap();
+        }
+        private void addEdge(Node dest, Double cost){
+            next.put(dest, cost);
+        }
+    }
+    public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+        double[] result = new double[queries.length];
+
+        // Build a map from the equations and values we have in input. 
+        HashMap<String, Node> map = new HashMap();  
+        for(int i = 0; i < equations.length; i++) {
+            String src = equations[i][0];
+            String dest = equations[i][1];
+            Node srcNode = !map.containsKey(src) ? new Node(src) : map.get(src);
+            Node destNode;
+            if(!map.containsKey(dest)){
+                destNode = new Node(dest);
+                map.put(dest, destNode);
+            } else {
+                destNode = map.get(dest);
+            }
+            // For 'a' and 'b'. Add the value of 'a/b'
+            srcNode.addEdge(destNode, values[i]);
+            map.put(src, srcNode);
+            
+            // For 'a' and 'b'. Also add the value of 'b/a'
+            destNode.addEdge(srcNode, 1/values[i]);
+            map.put(dest, destNode);
+        }
+        
+        for(int i = 0; i < queries.length; i++) {
+            String opr1 = queries[i][0];
+            String opr2 = queries[i][1];
+            if(!map.containsKey(opr1)||!map.containsKey(opr2)) {
+                result[i] = -1;
+                continue;
+            }
+            if(opr1.equals(opr2)) {
+                result[i] = 1;
+                continue;
+            }
+            // If result not already find the value
+            result[i] = findDiv(map.get(opr1), map.get(opr2));
+        }
+        return result;
+    }
+
+    private HashSet<Node> visited;
+    private double findDiv(Node src, Node dest){
+        if(src.next.containsKey(dest)){
+            return src.next.get(dest);
+        }
+        visited = new HashSet();
+        // Start DFS while calculating the division along the way
+        double division = findingDest(src, dest);
+        return division;
+    }
+    
+    private double findingDest(Node src, Node dest) {        
+        if(visited.contains(src)) return -1;
+        if(src.next.containsKey(dest)){
+            return src.next.get(dest);
+        }
+        visited.add(src);
+        double div = -1;
+        for(Map.Entry<Node, Double> e : src.next.entrySet()){
+            Node n1 = e.getKey();
+            double d = findingDest(n1, dest);
+            if(d==-1) continue;
+            div = d * e.getValue();
+            break;
+        }
+        return div;
+    }
+}
